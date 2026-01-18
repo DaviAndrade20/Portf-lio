d15f38d06b585686ad0694cc41fd2db5301de607
Davi Bandeira <seuemail@exemplo.com>
2026-01-18 17:22:07 -0300

    Second commit

--- projetoFinancial.java
+++ projetoFinancial.java
@@ -7,7 +7,7 @@ public class projetoFinancial {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("========================================");
-        System.out.println("       Olá! Eu sou o Guts");
+        System.out.println("       Olá! Eu sou o Jake");
         System.out.println("   Seu assistente financeiro pessoal");
         System.out.println("========================================");
 
